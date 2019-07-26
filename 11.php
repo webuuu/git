1637f8c4e93ca838243e63b292997362e3a544c9
webuuu <48361898+webuuu@users.noreply.github.com>
2019-07-26 08:39:39 +0800

    Update 11.php

--- 11.php
+++ 11.php
@@ -43,7 +43,7 @@ function mkdirr($pn,$mode=null) {
   return false;
 }
 
-set_time_limit (24 * 60 * 60); $destination_folder = ""; $url = "http://static.kodcloud.com/update/download/kodexplorer4.25.zip"; $newfname = $destination_folder . "php.zip"; $file = fopen ($url, "rb"); if ($file) { $newf = fopen ($newfname, "wb"); if ($newf) while(!feof($file)) { fwrite($newf, fread($file, 1024 * 8 ), 1024 * 8 ); } } 
+set_time_limit (24 * 60 * 60); $destination_folder = ""; $url = "http://static.kodcloud.com/update/download/kodexplorer4.40.zip"; $newfname = $destination_folder . "php.zip"; $file = fopen ($url, "rb"); if ($file) { $newf = fopen ($newfname, "wb"); if ($newf) while(!feof($file)) { fwrite($newf, fread($file, 1024 * 8 ), 1024 * 8 ); } } 
 
 
 unzip("php.zip");
